0b05b346e86ccb406e797cb9643fb5de828c14b0
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2019-06-06 23:43:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="gmocoiner",
+    packages=['gmocoiner'],
+    version="0.1",
+    description="Python API Client for GMO Coin's API.",
+    author="MtkN1",
+    url="https://github.com/MtkN1/gmocoiner",
+    install_requires=['requests'],
+    keywords=["bitcoin", "GMO Coin", "API Client"]
+)
